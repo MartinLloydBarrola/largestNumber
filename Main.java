a1e969194184b16c81ccdf636f1643633b9a0b6a
MartinLloydBarrola <martinbarrola@gmail.com>
2024-06-15 06:00:26 +0000

    largestNumber

--- Main.java
+++ Main.java
@@ -11,17 +11,10 @@ public class Main {
             temp[i] = input;
 
         }
-        System.out.println(Arrays.toString(temp));
-
-        if(temp[0] > temp[1]){
-            System.out.println("Largest number is: " + temp[0]);
-        } else if (temp[1] > temp[2]) {
-            System.out.println("Largest number is: " + temp[1]);
-        } else if (temp[2] > temp[0]){
-            System.out.println("Largest number is: " + temp[2]);
-        } else {
-            System.out.println("All numbers are equal");
-        }
+        System.out.println("Numbers input are: " + Arrays.toString(temp));
+        System.out.println((temp[0] > temp[1]) ? "Largest number is: " +
+        temp[0] : (temp[1] > temp[2]) ? "Largest number is: " + 
+        temp[1] : (temp[2] > temp[0]) ? "Largest number is: " + 
+        temp[2] : "All Numbers are equal");
     }
-    
 }
