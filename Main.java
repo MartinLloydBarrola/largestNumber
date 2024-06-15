ed661234e920da28775caacf3ab1333bb933a86c
MartinLloydBarrola <martinbarrola@gmail.com>
2024-06-15 06:11:56 +0000

    largestNumber using List

--- Main.java
+++ Main.java
@@ -1,20 +1,23 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] temp = new int[3];
-        for (int i = 0; i < 3; i++){
+       Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
             System.out.println("Input 3 numbers ");
             int input = scanner.nextInt();
-            temp[i] = input;
+            numbers.add(input);
 
         }
-        System.out.println("Numbers input are: " + Arrays.toString(temp));
-        System.out.println((temp[0] > temp[1]) ? "Largest number is: " +
-        temp[0] : (temp[1] > temp[2]) ? "Largest number is: " + 
-        temp[1] : (temp[2] > temp[0]) ? "Largest number is: " + 
-        temp[2] : "All Numbers are equal");
+        System.out.println(numbers);
+
+      System.out.println((numbers.get(0) > numbers.get(1)) ? "Largest number is: " +
+              numbers.get(0) : (numbers.get(1) > numbers.get(2)) ? "Largest number is: " +
+              numbers.get(1) : (numbers.get(2) > numbers.get(0)) ? "Largest number is: " +
+              numbers.get(2) : "All Numbers are equal");
     }
 }
